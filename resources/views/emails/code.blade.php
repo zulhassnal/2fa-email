c43186ac94493fe90c4052445848d092e9b7bb95
Zulhassnal Faisal Iman Mois <zulhassnal@gmail.com>
2023-10-11 12:21:00 +0800

    first commit

---
+++ resources/views/emails/code.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Code</title>
+</head>
+<body>
+    <h1>{{ $details['title'] }}</h1>
+    <p>Your code is : {{ $details['code'] }}</p>
+     
+    <p>Thank you</p>
+</body>
+</html>
